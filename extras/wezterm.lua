5e73f49d7e34e76be03071bb045604516d4f873e
ttak0422 <ttak0422@gmail.com>
2024-06-06 01:00:03 +0900

    feat: add wezterm

---
+++ extras/wezterm.lua
@@ -0,0 +1,37 @@
+return {
+	colors = {
+		foreground = "#b5D2a9",
+		background = "#2a2a2e",
+
+		cursor_bg = "#98b18e",
+		cursor_fg = "#98b18e",
+		cursor_border = "#98b18e",
+
+		selection_fg = "#98b18e",
+		selection_bg = "#7aa7b3",
+
+		scrollbar_thumb = "#313135",
+		split = "#313135",
+
+		ansi = {
+			"#2a2a2e",
+			"#d76e6e",
+			"#679f68",
+			"#d3c785",
+			"#789cbc",
+			"#b384b1",
+			"#7aa7b3",
+			"#98b18e",
+		},
+		brights = {
+			"#b0b7be",
+			"#ff6e6e",
+			"#98c593",
+			"#e0d7a3",
+			"#9bb9d4",
+			"#d0a3d0",
+			"#a0c8cc",
+			"#b5d2a9",
+		},
+	},
+}
